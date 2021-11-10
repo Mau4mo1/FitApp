2438febe59e04b49c7d313c5befb3285dc9d7d9e
Maurice <mauricederidder@outlook.com>
2021-11-10 21:27:29 +0100

    push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDetailComponent } from './pages/user/user-detail/user-detail.component';
+import { UserListComponent } from './pages/user/user-list/user-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'users', pathMatch: 'full', component: UserListComponent},
+  {path:'users/:id', pathMatch:'full',component: UserDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './pages/user/user-list/user-list.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { UserDetailComponent } from './pages/user/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    NavComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
